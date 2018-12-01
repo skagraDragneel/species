c92dab9a5b033541b384bb902bb2da026c27cd74
Collin Bolles <collinbolles@gmail.com>
2018-12-01 17:39:08 -0500

    Added comments and reintegrated main method

--- src/main.py
+++ src/main.py
@@ -5,16 +5,16 @@ from flask import Flask, request, render_template, jsonify
 from configparser import ConfigParser
 
 app = Flask(__name__)
-current_dir = os.path.abspath(os.path.dirname(__file__))
-default_model = os.path.join(current_dir, 'model/frozen_inference_graph.pb')
-default_labels = os.path.join(current_dir, 'model/mscoco_label_map.pbtxt')
-image_recognition = ImageRecognition(default_model, default_labels)
-default_config = os.path.join(current_dir, 'care.ini')
-config = ConfigParser()
-config.read(default_config)
+image_recognition = None
+config = None
 
 
-def get_care_url(names):
+def get_care_url(names: str) -> str:
+    '''
+    Goes through the list of recognized objects from the object detection and returns
+    the url for the first recognized object that has a corresponding url. If none
+    are found, returns empty string
+    '''
     print(names)
     for name in names:
         name.lower().replace(' ', '_')
@@ -25,26 +25,37 @@ def get_care_url(names):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
-@app.route('/main_page')
-def main_route():
+    '''
+    Main page where user send photo and views the corresponding instructions
+    '''
     return render_template('main.html')
 
 
 @app.route('/get_care', methods=['POST'])
-def name():
+def get_care():
+    '''
+    Used to get the care url.
+    Expects the request to contain a base64 encoded string containing the image to 
+    analyze.
+    Returns json with the url to the instructions
+    '''
     image_b64 = request.json['image_data'].split(',')[1]
     care_url = get_care_url(image_recognition.get_classes(image_b64))
     return jsonify({'url': care_url}), 200, {'ContentType': 'application/json'}
 
 
 def main():
+    '''
+    Handles running the app including initializing image detection with values passed
+    in from the user
+    '''
+    global image_recognition
+    global config
     # Default Values
     current_dir = os.path.abspath(os.path.dirname(__file__))
     default_model = os.path.join(current_dir, 'model/frozen_inference_graph.pb')
     default_labels = os.path.join(current_dir, 'model/mscoco_label_map.pbtxt')
+    default_config = os.path.join(current_dir, 'care.ini')
 
     # Argument Parsing
     parser = argparse.ArgumentParser(description='Run the species specific app')
@@ -52,13 +63,16 @@ def main():
                         help='location of the frozen tensorflow model')
     parser.add_argument('--labels', default=default_labels,
                         help='location of the tensorflow labels')
+    parser.add_argument('--config', default=default_config)
 
     args = parser.parse_args()
 
     image_recognition = ImageRecognition(args.frozen_model, args.labels)
-    print(image_recognition.get_classes('model/zebra.jpg'))
-    image_recognition.close()
+    config = ConfigParser()
+    config.read(default_config)
+
+    app.run(debug=True, host='0.0.0.0')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    main()
